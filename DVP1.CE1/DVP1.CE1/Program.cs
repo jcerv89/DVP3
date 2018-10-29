5ed27f7781d583806558f4a828eeaaca3da7fa68
jcerv89 <jmcervenec@student.fullsail.edu>
2018-10-29 13:23:28 -0400

    new menu

---
+++ DVP1.CE1/DVP1.CE1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVP1.CE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome user to program with intructions
+            Console.WriteLine("PLease select the program you want to run and press enter.");
+            Console.WriteLine();//Space between outputs
+            
+            //Displays the menu choices
+            Console.WriteLine("[1] Swap Name\r\n[2] Backwards\r\n[3] Age Convert\r\n[4] Temp Convert\r\n[5] Big Blue Fish\r\n[0] Exit");
+            
+            //User prompt for selection
+            string userChoiceString = Console.ReadLine();
+            
+            //Variable to store user number input
+            int userChoice;
+           
+            //Loop to varify user input
+            while (!int.TryParse(userChoiceString, out userChoice) ||(userChoice > 5) || (string.IsNullOrWhiteSpace(userChoiceString)))
+            {
+                Console.WriteLine("Please enter a valid option");
+                userChoiceString = Console.ReadLine();
+            }
+
+        }
+    }
+}
